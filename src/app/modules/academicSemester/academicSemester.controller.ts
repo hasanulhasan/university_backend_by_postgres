10a326bf485db8421c39d65537654a2e654b4d15
hasanulhasan <hasanlink71@yahoo.com>
2023-08-24 09:28:44 +0600

    validation added and response modified

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -7,21 +7,19 @@ import httpStatus from "http-status";
 const insertIntoDB = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const result = await AcademicSemesterService.insertIntoDB(req.body);
-    // sendResponse<AcademicSemester>(res, {
-    //   statusCode: httpStatus.OK,
-    //   success: true,
-    //   message: "Academic Semester Created",
-    //   data: result
-    // })
-    res.send({
-      status: 'success',
+    sendResponse<AcademicSemester>(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "Academic Semester Created",
       data: result
     })
-
+    
   } catch (error) {
     next(error)
   }
 }
+
+
 
 export const AcademicSemesterController = {
   insertIntoDB
--- src/app/modules/academicSemester/academicSemester.routes.ts
+++ src/app/modules/academicSemester/academicSemester.routes.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import { AcademicSemesterController } from './academicSemester.controller';
+import { AcademicSemesterValidation } from './academicSemesterValidation';
+import validateRequest from '../../middlewares/validateRequest';
 
 const router = express.Router();
 
-router.post('/', AcademicSemesterController.insertIntoDB);
+router.post('/',validateRequest(AcademicSemesterValidation.create), AcademicSemesterController.insertIntoDB);
 
 export const AcademicSemesterRoutes = router;
